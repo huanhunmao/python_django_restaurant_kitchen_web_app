6424bc4c7d3d9a21a43780d6dc169a1d7331c35f
Marxu <fuhaojun@52tt.com>
2024-07-14 10:11:02 +0800

    feat: context in django , 可以拿到 数据库数据 然后 index 展示

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -8,6 +8,14 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by('-date_created')
     template_name = 'index.html'
 
+    def get_context_data(self):
+        context = {
+            'meals': ['apple', 'milk'],
+            'ice': ['ppx', 'cc']
+        }
+
+        return context
+
 
 class MenuItemDetail(generic.DetailView):
     model = Item
