ca015870e61022132125cf86058e7688e3c6b9d8
Marxu <fuhaojun@52tt.com>
2024-07-14 14:44:16 +0800

    feat: 拿到数据库数据，并循环♻️展示出数据

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 
 # Create your views here.
@@ -8,11 +8,9 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by('-date_created')
     template_name = 'index.html'
 
-    def get_context_data(self):
-        context = {
-            'meals': ['apple', 'milk'],
-            'ice': ['ppx', 'cc']
-        }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meals'] = MEAL_TYPE
 
         return context
 
